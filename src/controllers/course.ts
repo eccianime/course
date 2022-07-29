fab411403105bd19528ab6d800658bcccb1cd221
Jean Paul Rojas <eccianime@gmail.com>
2022-07-29 13:13:39 -0300

    Conectado -Meus Cursos- com API

--- src/controllers/course.ts
+++ src/controllers/course.ts
@@ -1,17 +1,24 @@
+import { FindOptions } from "sequelize";
 import { Course, Enroll } from "../models";
 import ErrorResponse from "../utils/errorResponse";
 
 export const getEnrolledCourses = async ( req: any, res: any, next: any ) => {
-    const { user_id } = req.body;
+    const { user_id, limit } = req.query;
+    console.log({user_id, limit})
     if( !user_id ) return next(new ErrorResponse('Deve especificar do qual usuário deseja ver os Cursos.', 400));
-    const courses = await Enroll.findAll({ 
-        where: {
-            user_id
-        },
-        include: Course
-    });
+
+    const options: FindOptions = {
+        where: { user_id },
+        include: [{ model: Course, required: true }]
+    }
+    if( limit ){
+        options.limit = limit
+    }
+
+    const courses = await Enroll.findAndCountAll(options);
     res.status(200).json({
         success: true,
-        data: courses
+        data: courses.rows,
+        total: courses.count
     })
 }
--- src/routes/course.ts
+++ src/routes/course.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { getEnrolledCourses } from '../controllers/course';
 const router = express.Router();
 
-router.post('/enrolled', getEnrolledCourses);
+router.get('/enrolled', getEnrolledCourses);
 
 export default router;
