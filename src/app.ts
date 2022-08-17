a8cda00e4ba96d62ee9b684e8786a243370ee276
Jean Paul Rojas <eccianime@gmail.com>
2022-08-17 08:40:51 -0300

    Verificando um console.log

--- src/app.ts
+++ src/app.ts
@@ -28,6 +28,7 @@ const app = express();
 
 ( async () => {
     try {
+        console.log(process.env.PG_CONNECTION_DATABASE);
         await sequelize.authenticate();
         console.log( colors.green.bold('Conectado ao servidor com sucesso.'));
     } catch (error) {
