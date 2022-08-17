8764d5875c00fa792f1b5836d529c46baf16d953
Jean Paul Rojas <eccianime@gmail.com>
2022-08-17 09:28:52 -0300

    Corrigido SSL

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -6,8 +6,13 @@ const sequelize = new Sequelize({
     username: process.env.PG_CONNECTION_USER,
     password: process.env.PG_CONNECTION_PASS,
     port: 5432,
-    ssl: true,
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false 
+        }
+    },
 });
  
 export default sequelize;
