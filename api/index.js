3660cc7cfd52d212c5356223099dd377eeba4f50
Jean Paul Rojas <eccianime@gmail.com>
2022-12-12 19:40:09 -0300

    src trocado por api

---
+++ api/index.js
@@ -0,0 +1,72 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const colors_1 = __importDefault(require("colors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const hpp_1 = __importDefault(require("hpp"));
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+// import https from 'https';
+const http_1 = __importDefault(require("http"));
+const error_1 = __importDefault(require("../src/middleware/error"));
+console.log("NODE_ENV1: ", process.env.NODE_ENV);
+dotenv_1.default.config();
+const auth_1 = __importDefault(require("../src/routes/auth"));
+const course_1 = __importDefault(require("../src/routes/course"));
+const contents_1 = __importDefault(require("../src/routes/contents"));
+const notFound_1 = __importDefault(require("../src/routes/notFound"));
+const dbConnection_1 = __importDefault(require("../src/config/dbConnection"));
+const app = (0, express_1.default)();
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield dbConnection_1.default.authenticate();
+        console.log(colors_1.default.green.bold("Conectado ao servidor com sucesso."));
+    }
+    catch (error) {
+        console.log(colors_1.default.red.inverse.bold("Erro ao tentar conectar ao servidor"));
+        console.log(error);
+    }
+}))();
+app.use(express_1.default.json({ limit: "50mb" }));
+app.use((0, express_fileupload_1.default)());
+app.use((0, helmet_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)());
+app.use((0, express_rate_limit_1.default)({
+    windowMs: 10 * 60 * 1000,
+    max: 1000,
+}));
+app.use((0, hpp_1.default)());
+if (process.env.NODE_ENV === "development") {
+    app.use((0, morgan_1.default)("dev"));
+}
+app.use("/v1/contents", contents_1.default);
+app.use("/v1/courses", course_1.default);
+app.use("/v1/auth", auth_1.default);
+app.use("/", notFound_1.default);
+app.use(error_1.default);
+const PORT = process.env.PORT || 4632;
+// const SECURE_PORT = process.env.PORT || 8000;
+const server = http_1.default.createServer(app);
+console.log("NODE_ENV2: ", process.env.NODE_ENV);
+server.listen(PORT, () => console.log(colors_1.default.yellow.bold(`[${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}] Servidor executando em modo ${process.env.NODE_ENV} no porto ${PORT}`)));
+process.on("unhandledRejection", (error) => {
+    console.log(colors_1.default.red.inverse(`Error: ${error.message}`));
+    server.close(() => process.exit(1));
+});
